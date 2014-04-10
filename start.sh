2d62659a5c6da63daad73747d2c8cd424b66c8cd
FreemanZY <git@allto.us>
2014-04-10 14:23:59 +0800

    降版本到官方1.8 final(2010-10-31)

--- start.sh
+++ start.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-# If you see this file in an editor you may have to set the executable bit and execute it as script in a terminal
-
-# This file will start the Mobile Atlas Creator with custom memory settings for
+# This file will start the TrekBuddy Atlas Creator with custom memory settings for
 # the JVM. With the below settings the heap size (Available memory for the application)
-# will range from 64 megabyte up to 1024 megabyte.
+# will range from 64 megabyte up to 512 megabyte.
 
-java -Xms64m -Xmx1024M -jar Mobile_Atlas_Creator.jar
+java -Xms64m -Xmx512M -jar Mobile_Atlas_Creator.jar
